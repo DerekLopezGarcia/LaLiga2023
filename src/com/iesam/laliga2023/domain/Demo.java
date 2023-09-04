8d4d0461f184b32393ac9dff88afe36048b27980
DerekLopezGarcia <115727176+DerekLopezGarcia@users.noreply.github.com>
2023-09-04 19:40:19 +0200

    Se han creado 2 equipos y se muestra la informacion de 1 por pantalla

---
+++ src/com/iesam/laliga2023/domain/Demo.java
@@ -0,0 +1,14 @@
+package com.iesam.laliga2023.domain;
+
+public class Demo {
+    private Integer id;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
